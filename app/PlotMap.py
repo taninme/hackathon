ba619b19c38fd0dc91a8b7ed12f372f74fa27522
Larry Liu <Larry@Larrys-MacBook-Pro.local>
2015-07-25 14:35:29 -0400

    missign gile

--- app/PlotMap.py
+++ app/PlotMap.py
@@ -1,10 +1,7 @@
-import heatmap
-
-
 class plottool():
 	def heatmap(self, input):
-		hm = heatmap.Heatmap()
-
-		pts = [(x.lat, x.lng) for x in input if x.lat > 0 ]
-		return pts
+		
+		LNG = [x.lng for x in input]
+		LAT = [x.lat for x in input]
+		return LNG, LAT
 
